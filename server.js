269ce2b5fc839502ff7e9a4263c58b99726700b0
„michellesaric“ <msaric03@fesb.hr>
2022-03-23 09:14:12 +0100

    Added custom error handling middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 connectDB();
 
@@ -10,11 +11,15 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 4000;
 
-const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
 
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Logged Error: ${err}`);
   server.close(() => process.exit(1));
-})
+});
